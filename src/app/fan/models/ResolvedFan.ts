08db52b9bc73205f6c92284f4a938c45632201e9
Santos Pierre <santospierre1305@gmail.com>
2022-07-20 16:55:22 +0200

    Remove Unused Code 💩

--- src/app/fan/models/ResolvedFan.ts
+++
@@ -1,6 +0,0 @@
-import { Fan } from './Fan';
-
-export type ResolvedFan = {
-  fan: Fan;
-  error?: string;
-};
--- src/app/fan/resolver/FanResolver.ts
+++ src/app/fan/resolver/FanResolver.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
-import { Observable, of } from 'rxjs';
 import { Fan } from '../models/Fan';
 import { FanService } from '../services/fan.service';
 
 @Injectable({ providedIn: 'root' })
 export class FanResolver implements Resolve<Fan | Promise<boolean>> {
   constructor(private _fanService: FanService, private router: Router) {}
-
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     try {
       const fan = this._fanService.getFanById(route.paramMap.get('id'));
